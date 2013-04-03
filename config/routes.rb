311f9f485e7cb20dbb8b3342773861c302f97408
Salman Ahmad <salman@salmanahmad.com>
2013-04-03 14:40:12 -0400

    Start.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ GradeBook::Application.routes.draw do
 
 
   get "welcome/index"
+  root to: "welcome#index"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
